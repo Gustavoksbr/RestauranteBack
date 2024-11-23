652e3484975cbd0369bd5f116c590f5a75166286
Gustavoksbr <gustavosalesi@hotmail.com>
2024-11-23 00:43:27 -0300

    Update AB#87 dependencias e config para autenticaco do usuario com jwt
    obs: a partir de agora, cada um que for clonar o projeto, terá de criar um arquivo .env,.app.pub e app.key em src/main/resources/

--- src/main/java/com/scrum/restaurante/infra/config/security/SecurityConfig.java
+++ src/main/java/com/scrum/restaurante/infra/config/security/SecurityConfig.java
@@ -1,4 +1,19 @@
 package com.scrum.restaurante.infra.config.security;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+    @Value("${jwt.public.key}")
+    public RSAPublicKey key;
+    @Value("${jwt.private.key}")
+    public RSAPrivateKey priv;
+
+
 }
