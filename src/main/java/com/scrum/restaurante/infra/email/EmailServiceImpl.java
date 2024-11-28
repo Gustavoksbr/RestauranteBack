00427881b7b32a052693d943019abe426987dd5f
Gustavoksbr <gustavosalesi@hotmail.com>
2024-11-27 23:10:18 -0300

    Update AB#98 Deixando metodo de envio de email assincrono

--- src/main/java/com/scrum/restaurante/infra/email/EmailServiceImpl.java
+++ src/main/java/com/scrum/restaurante/infra/email/EmailServiceImpl.java
@@ -8,21 +8,23 @@ import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
+import java.util.concurrent.CompletableFuture;
+
 @Service
 public class EmailServiceImpl implements EmailService {
     private static final String FROM_EMAIL = "noreply@emaill.com";
     @Autowired
     private MailConfig mailConfig;
     @Override
-    public void sendEmail(Email email) {
+public void sendEmail(Email email) {
+    CompletableFuture.runAsync(() -> {
         JavaMailSender javaMailSender = this.mailConfig.createMailSender(email.getProvider().toString());
         SimpleMailMessage message = new SimpleMailMessage();
         message.setFrom(FROM_EMAIL);
         message.setTo(email.getTo());
         message.setSubject(email.getSubject());
         message.setText(email.getBody());
-
         javaMailSender.send(message);
-    }
-}
+    });
+}}
 
